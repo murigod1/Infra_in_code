f1f7cb920e04b1474e66d82ef5595295652e19fa
Murilo Jesus Sales <81323368+murigod1@users.noreply.github.com>
2023-11-15 00:45:24 -0300

    Projeto finalizado

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami = "ami-08e2c1a8d17c2fe17"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+  # user_data = <<-EOF
+  #                 #!/bin/bash
+  #                 cd /home/ubuntu
+  #                 echo "<h1>Feito com terraform</h1>" > index.html
+  #                 nohup busybox httpd -f -p 8080 &
+  #                 EOF
+  tags = {
+    Name = "Aws ansible Python"
+  }
+}
